e07597855fc3caed6cbf4cae79dfce9552587187
Aalok Pandit <51901277+fellowpandit@users.noreply.github.com>
2023-12-28 15:11:30 +0530

    Update p6.tcl

--- p6.tcl
+++ p6.tcl
@@ -1,3 +1,5 @@
+#to execute it's "ns p6.tcl"
+
 #Setting the default parameters
 
 set val(chan) Channel/WirelessChannel
@@ -7,27 +9,27 @@ set val(mac) Mac/802_11
 set val(ifq) Queue/DropTail/PriQueue
 set val(ll) LL
 set val(ant) Antenna/OmniAntenna
-set val(x) 500
-set val(y) 500
+set val(x) 400
+set val(y) 400
 set val(ifqlen) 50
 set val(nn) 2
 set val(stop) 20.0
 set val(rp) DSDV
 
-set ns_ [new Simulator]
-set tracefd [open 001.tr w]
-$ns_ trace-all $tracefd
-set namtrace [open 001.nam w]
-$ns_ namtrace-all-wireless $namtrace $val(x) $val(y)
+set ns [new Simulator]
+set tf [open p6.tr w]
+$ns trace-all $tf
+set nf [open p6.nam w]
+$ns namtrace-all-wireless $nf $val(x) $val(y)
 
 set prop [new $val(prop)]
 set topo [new Topography]
-$topo load-flatgrid $val(x) $val(y)
+$topo load_flatgrid $val(x) $val(y)
 
 create-god $val(nn)
 
 #Node Configuration
-$ns_ node-config -adhocRouting $val(rp)\
+$ns node-config -adhocRouting $val(rp)\
 					-llType $val(ll)\
 					-macType $val(mac)\
 					-ifqType $val(ifq)\
@@ -43,38 +45,58 @@ $ns_ node-config -adhocRouting $val(rp)\
 					
 #Creating Nodes
 for {set i 0} {$i < $val(nn)} {incr i} {
-	set node_($i) [$ns_ node]
+	set node_($i) [$ns node]
 	$node_($i) random-motion 0
 }
 					
-#Initial Positions of Nodes
+#Initial Positions of Nodes Spread
 for {set i 0} {$i < $val(nn)} {incr i} {
-	$ns_ initial_node_pos $node_($i) 40
+	$ns initial_node_pos $node_($i) 40
 }
 
+$node_(0) set x 10.0
+$node_(0) set y 10.0
+
+$node_(1) set x 200.0
+$node_(1) set y 200.0
+
 #Topology Design
-$ns_ at 1.1 "$node_(0) setdest 310.0 10.0 20.0"
-$ns_ at 1.1 "$node_(0) setdest 10.0 310.0 20.0"
+$ns at 1.1 "$node_(0) setdest 300.0 300.0 40.0"
 
 #Generating Taffic
 set tcp0 [new Agent/TCP]
 set sink0 [new Agent/TCPSink]
-$ns_ attach-agent $node_(0) $tcp0
-$ns_ attach-agent $node_(1) $sink0
-$ns_ connect $tcp0 $sink0
+$ns attach-agent $node_(0) $tcp0
+$ns attach-agent $node_(1) $sink0
+$ns connect $tcp0 $sink0
+
 set ftp0 [new  Application/FTP]
 $ftp0 attach-agent $tcp0
-$ns_ at 1.0 "$ftp0 start"
-$ns_ at 18.0 "$ftp0 stop"
+
+$ns at $val(stop) "finish"
+$ns at 1.0 "$ftp0 start"
+$ns at 18.0 "$ftp0 stop"
+
+proc finish {} {
+	global ns tf nf
+	close $nf
+	close $tf
+	puts "Simulator Starting..."
+	exec nam p6.nam &
+	exit 0
+}
 
 #Simulation Termination
-for {set i 0} {$i < $val(nn)} {incr i} s{
-	$ns_ at $val(stop) "$node_($i) reset";
+for {set i 0} {$i < $val(nn)} {incr i} {
+	$ns at $val(stop) "$node_($i) reset";
 }
-$ns_ at $val(stop) "puts\"NS EXITING...\"; $ns_halt"
+$ns at $val(stop) "puts\"NS EXITING...\"; $ns halt"
 
 puts "Starting Simulation"
-$ns_ run
+$ns run
+
+
+
 
 
 
